594b66dbd2686573d5b384ad59422b4990975734
Michael Withers <michaelwithers@Michaels-Air.attlocal.net>
2018-10-22 00:47:34 -0400

    changes

--- cow.js
+++ cow.js
@@ -1,8 +1,8 @@
 
 var cow = {
-  yAxis: 510,
-  xAxis: 820,
-  direction: 1
+  yAxis: 220,
+  xAxis: 52,
+  direction: 2
 }
 
 var commands = [];
@@ -29,106 +29,113 @@ function backward(){
 
 
 
+
+  //loops through array of commands and executes
 function execute(){
 
-
 for (var i = 0; i < commands.length; i++){
-
-
-
+ 
 if(commands[i] == "f" && cow.direction == 1){
-cow.yAxis -= 60;
+setTimeout(function(){ cow.yAxis -= 60; }, i * 500);
 }
+//forward button
 else if(commands[i] == "f" && cow.direction == 2){
-  cow.xAxis += 60;
+    setTimeout(function(){ cow.xAxis += 60; }, i * 500);
   }
   else if(commands[i] == "f" && cow.direction == 3){
-    cow.yAxis += 60;
-    }
+    setTimeout(function(){ cow.yAxis += 60; }, i * 500);
+  }
     else if(commands[i] == "f" && cow.direction == 4){
-      cow.xAxis -= 60;
-      }
+      setTimeout(function(){ cow.xAxis -= 60; }, i * 500);
+    }
 
-
+//back button
 if(commands[i] == "b" && cow.direction == 1){
-cow.yAxis += 60;
+  setTimeout(function(){cow.yAxis += 60; }, i * 500);
+
 }else if(commands[i] == "b" && cow.direction == 2){
-  cow.xAxis -= 60;
+  setTimeout(function(){cow.xAxis -= 60; }, i * 500);
+
   }else if(commands[i] == "b" && cow.direction == 3){
-    cow.yAxis -= 60;
-    }else if(commands[i] == "b" && cow.direction == 4){
-      cow.xAxis += 60;
-      }
+    setTimeout(function(){cow.yAxis -= 60; }, i * 500);
 
+  }else if(commands[i] == "b" && cow.direction == 4){
+    setTimeout(function(){cow.xAxis += 60; }, i * 500);
+  }
 
+//right button
 
       if(commands[i] == "r" && cow.direction == 1){
-        document.getElementById('cow').style.transform = "rotate(90deg)";
+        setTimeout(function(){ document.getElementById('cow').style.transform = "rotate(90deg)"; }, i * 500);
         cow.direction = 2; //2 in east
-        console.log(cow.direction);
-        
         }
 
        else if(cow.direction == 2 && commands[i] == "r"){
-          document.getElementById('cow').style.transform = "rotate(180deg)";
+        setTimeout(function(){ document.getElementById('cow').style.transform = "rotate(180deg)"; }, i * 500);
           cow.direction = 3; //3 is south
-          console.log(cow.direction);
-  
-          }
+        }
         
         else if(cow.direction == 3 && commands[i] == "r"){
-          document.getElementById('cow').style.transform = "rotate(270deg)";
+          setTimeout(function(){ document.getElementById('cow').style.transform = "rotate(270deg)"; }, i * 500);
           cow.direction = 4; //4 is west
-          console.log(cow.direction);
-  
-          }
+         }
+
           else if(cow.direction == 4 && commands[i] == "r"){
-            document.getElementById('cow').style.transform = "rotate(360deg)";
+            setTimeout(function(){ document.getElementById('cow').style.transform = "rotate(360deg)"; }, i * 500);
             cow.direction = 1;
-            console.log(cow.direction);
-    
-            }  
+          }  
+
+
+
+
+//left button
 
             if(commands[i] == "l" && cow.direction == 1){
-              document.getElementById('cow').style.transform = "rotate(270deg)";
+              setTimeout(function(){ document.getElementById('cow').style.transform = "rotate(270deg)"; }, i * 500);
               cow.direction = 4; 
-              console.log(cow.direction);
               
-              }
+                }
       
              else if(cow.direction == 2 && commands[i] == "l"){
-                document.getElementById('cow').style.transform = "rotate(360deg)";
-                cow.direction = 1; 
-                console.log(cow.direction);
+              setTimeout(function(){ document.getElementById('cow').style.transform = "rotate(360deg)"; }, i * 500);
+              cow.direction = 1; 
         
                 }
               
               else if(cow.direction == 3 && commands[i] == "l"){
-                document.getElementById('cow').style.transform = "rotate(90deg)";
+                setTimeout(function(){ document.getElementById('cow').style.transform = "rotate(90deg)"; }, i * 500);
                 cow.direction = 2; 
-                console.log(cow.direction);
         
                 }
                 else if(cow.direction == 4 && commands[i] == "l"){
-                  document.getElementById('cow').style.transform = "rotate(180deg)";
+                  setTimeout(function(){ document.getElementById('cow').style.transform = "rotate(180deg)"; }, i * 500);
                   cow.direction = 3;
-                  console.log(cow.direction);
           
                   } 
 
-
-
-
-
-            window.setInterval(function makeStuffMove() {
-              document.getElementById("cow").style.top = (cow.yAxis) + "px";
-              document.getElementById("cow").style.left = (cow.xAxis) + "px";
-              
-              }, 60/1000);
-
+                  //used to help find collision position
+                  console.log("x axis = " + cow.xAxis + " " + "y axis = " + cow.yAxis);
 }
 
 commands = []; //clear the array of commands
  }
+
+ window.setInterval(function makeStuffMove() {
+  
+  //below statements check for fence collision 
+  if (cow.yAxis < 160){
+    cow.yAxis = 100;
+  }
+  if (cow.yAxis > 580){
+      cow.yAxis = 580;
+   }
+
+   if (cow.xAxis < 52){
+     cow.xAxis = 52;
+   }
+
+  document.getElementById("cow").style.left = (cow.xAxis) + "px";
+  document.getElementById("cow").style.top = (cow.yAxis) + "px";
+  }, 500);
 
 
